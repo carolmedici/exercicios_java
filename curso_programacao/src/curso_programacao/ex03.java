3b31960322d12e4c60ded371d59ee36c5a69ba8c
carolmedici <carolina.obstetriz@gmail.com>
2023-06-13 23:47:22 -0300

    exercício 03 - calcular diferença do produto

---
+++ curso_programacao/src/curso_programacao/ex03.java
@@ -0,0 +1,27 @@
+package curso_programacao;
+import java.util.Scanner;
+
+public class ex03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+				
+				Scanner sc = new Scanner(System.in);
+				
+				int A, B, C, D;
+				
+				A = sc.nextInt();
+				B = sc.nextInt();
+				C = sc.nextInt();
+				D = sc.nextInt();
+
+				int diferenca = (A*B)-(C*D);
+				
+				System.out.println("DIFERENÇA = " + diferenca);
+				
+						sc.close();
+							
+				
+		}
+		}
