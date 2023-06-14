86a36456d40ecc4ae95d838a01e23dbc46dcd50d
carolmedici <carolina.obstetriz@gmail.com>
2023-06-13 22:32:36 -0300

    exercicio 01 - soma

---
+++ curso_programacao/src/curso_programacao/ex01.java
@@ -0,0 +1,23 @@
+package curso_programacao;
+import java.util.Scanner;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	
+		Scanner sc = new Scanner(System.in);
+		int valor1, valor2, soma;
+		valor1 = sc.nextInt();
+		valor2 = sc.nextInt();
+		soma = valor1 + valor2;
+		
+				System.out.printf("SOMA = " + soma);
+		
+				sc.close();
+		
+		
+}
+}
