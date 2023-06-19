cb1a9a3d5d4445fb2aa48507851ec06a7cf5bbcf
carolmedici <carolina.obstetriz@gmail.com>
2023-06-18 22:45:12 -0300

    exercicio 18 - Teste de intervalo

---
+++ curso_programacao/src/curso_programacao/ex18.java
@@ -0,0 +1,32 @@
+package curso_programacao;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ex18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double valor = sc.nextDouble();
+		
+		if(valor >=0 && valor <= 25) {
+			System.out.println("Intervalo [0,25]");
+		}else if(valor > 25 && valor <=50) {
+			System.out.println("Intervalo (25,50]");
+		}else if(valor > 50 && valor<=75) {
+			System.out.println("Intervalo (50,75]");
+		}
+		else if(valor > 75 && valor<=100) {
+			System.out.println("Intervalo (75,100]");
+		}
+		else {
+			System.out.println("Fora de intervalo");
+		}
+		
+		
+		sc.close();
+	}
+
+}
