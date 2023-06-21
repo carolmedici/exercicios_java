5269dbeabdb20de0ab6b5c0563aa9ed69a4b0c1c
carolmedici <carolina.obstetriz@gmail.com>
2023-06-20 21:56:24 -0300

    exercício 20 - Imprimir apenas valores ímpares

---
+++ curso_programacao/src/curso_programacao/ex20.java
@@ -0,0 +1,26 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class ex20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int valor = sc.nextInt();
+		int i;
+		
+		
+		for(i=0; i <= valor; i++) {
+			if ( (i % 2) != 0) {
+			
+			System.out.println(i);
+		}}
+			
+			
+			sc.close();
+	}
+
+}
