f9decdb87865087f27cccf87cda30ae4f250a193
carolmedici <carolina.obstetriz@gmail.com>
2023-06-27 23:00:16 -0300

    :bug: ajustando código

--- curso_programacao/src/curso_programacao/ex15.java
+++ curso_programacao/src/curso_programacao/ex15.java
@@ -1,10 +1,26 @@
 package curso_programacao;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ex15 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault (Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+	
+		int senha = sc.nextInt();
+		while (senha != 2002) {
+			System.out.println("Senha Invalida");
+			senha = sc.nextInt();
 
+		}
+		System.out.println("Acesso Permitido");
+
+		sc.close();
+		
+		
 	}
 
 }
