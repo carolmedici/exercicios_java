28805458f2a15105ab2b5862cf9ea6685db53561
carolmedici <carolina.obstetriz@gmail.com>
2023-09-01 22:38:49 -0300

    Exercício - Resto de divisão

---
+++ curso_programacao/src/curso_programacao/ex38.java
@@ -0,0 +1,27 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class ex38 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite 2 números");
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int maior = Math.max(x, y);
+		int menor = Math.min(x, y);
+				
+		for(int i = menor; i <= maior ; i++) {
+			if(i % 5 == 2 || i % 5 == 3 ) {
+				if(i>0) {
+				System.out.println(i);
+				}
+			}
+		}
+				
+		sc.close();
+	}
+}
