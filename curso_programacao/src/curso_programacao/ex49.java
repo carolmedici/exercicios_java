d8953c8290b7846732413aae704ca6ba5ea9b733
carolmedici <carolina.obstetriz@gmail.com>
2023-09-19 06:39:43 -0300

    Encontrando as posições adjacentes

--- curso_programacao/src/curso_programacao/ex49.java
+++ curso_programacao/src/curso_programacao/ex49.java
@@ -1,23 +1,33 @@
 package curso_programacao;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ex49 {
 
 	public static void main(String[] args) {
 	
+		int acheiLinha = 0;
+		int acheiColuna = 0;
+		int procuraColuna = 0;
+		int index = 0;
 		
-				try (Scanner sc = new Scanner(System.in)) {
-					int index = 0;
-							
-					System.out.println("Qual será o número de colunas?");
-					int xColuna = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Qual será o número de colunas?");
+		int xColuna = sc.nextInt();
+		
+		System.out.println("Qual será o número de linhas?");
+		int xLinha = sc.nextInt();
+		
+		String[][] tabela = new String[xLinha][xColuna]; 
+		
+		int indexArray = 0;
+		String[] arrayPosicoes = new String[4];
+		
+		int linha = 0;
+		
+				try  {
 					
-					System.out.println("Qual será o número de linhas?");
-					int xLinha = sc.nextInt();
-					
-					String[][] tabela = new String[xLinha][xColuna]; 
-					
-					int linha = 0;
 					
 					for(linha = 0; linha < xLinha; linha++ ) {
 						System.out.println("Digite os textos da linha " + (linha+1));
@@ -47,28 +57,43 @@ public class ex49 {
 					System.out.println("Qual texto deseja analisar"); 
 					
 					String posicao = sc.next();
-					int acheiLinha = 0;
-					int acheiColuna = 0;
-					int procuraColuna = 0;
+				
 					
 					for(int procuraLinha = 0; procuraLinha < xLinha; procuraLinha++) {
 						for(procuraColuna = 0; procuraColuna < xColuna; procuraColuna++) {
 							if(tabela[procuraLinha][procuraColuna].equals(posicao)) {
 								acheiColuna = procuraColuna;
-								acheiLinha = procuraLinha;}
+								acheiLinha = procuraLinha;
 							}
-						
+						}
 					}
 					
 					System.out.println("O texto desejado está na posição: (" + acheiLinha + ", " + acheiColuna + ")");
-					
-					sc.close();
-					
-					
-				}catch(Exception ArrayIndexOutOfBoundsException){
+										
+				}
+				catch(Exception ArrayIndexOutOfBoundsException){
 					System.out.println("Erro: " + ArrayIndexOutOfBoundsException.getMessage());
 				}
 				
+				try {
+					
+					arrayPosicoes[0] = tabela[acheiLinha-1][acheiColuna];
+					arrayPosicoes[1] = tabela[acheiLinha][acheiColuna+1];
+					arrayPosicoes[2] = tabela[acheiLinha+1][acheiColuna];
+					arrayPosicoes[3] = tabela[acheiLinha][acheiColuna-1];
+						
+					System.out.println("Acima está: " + arrayPosicoes[0]);
+					System.out.println("À direita: " + arrayPosicoes[1]);
+					System.out.println("Abaixo: " + arrayPosicoes[2]);
+					System.out.println("À esquerda: " + arrayPosicoes[3]);
+					
+				
+				}
+				catch(Exception ArrayIndexOutOfBoundsException){
+				System.out.println("Não é possível imprimir as posições adjacentes >> Fora do limite da matriz." );
+				}
+			
+			sc.close();
 			}
 
 		}
