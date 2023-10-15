23b67a347e9dec3e38ea2d0ccdf90345db2906b8
carolmedici <carolina.obstetriz@gmail.com>
2023-10-14 22:23:43 -0300

    add um while, para continuar perguntando até valor inserido  estar certo

--- curso_programacao/src/curso_programacao/ex54.java
+++ curso_programacao/src/curso_programacao/ex54.java
@@ -1,40 +1,45 @@
 package curso_programacao;
 import java.util.Locale;
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class ex54 {
 
-	    public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-		        
-	        sc.useLocale(Locale.US);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	        System.out.print("Digite o valor com duas casas decimais (Ex: 4.00): ");
-	        try {
-	        
-	        double valor = sc.nextDouble();
+        sc.useLocale(Locale.US);
 
-	        int[] notas = { 100, 50, 20, 10, 5, 2 };
-	        double[] moedas = { 1, 0.5, 0.25, 0.1, 0.05, 0.01 };
-	        System.out.println();
-	        System.out.println("NOTAS:");
-	        for (int nota : notas) {
-	            int quantidadeNotas = (int) (valor / nota);
-	            valor %= nota;
-	            
-	            System.out.printf("%d nota(s) de R$ %.2f\n", quantidadeNotas, (double) nota);
-	        }
-	        System.out.println();
-	        System.out.println("MOEDAS:");
-	        for (double moeda : moedas) {
-	            int quantidadeMoedas = (int) (valor / moeda);
-	            valor %= moeda;
-	            System.out.printf("%d moeda(s) de R$ %.2f\n", quantidadeMoedas, moeda);
-	        }
-	        }
-	        catch (Exception e){
-	        	System.out.println(">> Erro << Digite o número com '.' e não ','");
-	        }
-	        sc.close();
-	    }
-	}
+        boolean valorCerto = false;
+        double valor = 0;
+
+        while (!valorCerto) {
+            System.out.print("Digite o valor com duas casas decimais (Ex: 4.00): ");
+            try {
+                valor = sc.nextDouble();
+                valorCerto = true;
+            } catch (Exception e) {
+                System.out.println(">> Erro << Digite o número com '.' e não ','");
+                sc.nextLine(); 
+            }
+        }
+
+        int[] notas = {100, 50, 20, 10, 5, 2};
+        double[] moedas = {1, 0.5, 0.25, 0.1, 0.05, 0.01};
+        System.out.println();
+        System.out.println("NOTAS:");
+        for (int nota : notas) {
+            int quantidadeNotas = (int) (valor / nota);
+            valor %= nota;
+            System.out.printf("%d nota(s) de R$ %.2f\n", quantidadeNotas, (double) nota);
+        }
+        System.out.println();
+        System.out.println("MOEDAS:");
+        for (double moeda : moedas) {
+            int quantidadeMoedas = (int) (valor / moeda);
+            valor %= moeda;
+            System.out.printf("%d moeda(s) de R$ %.2f\n", quantidadeMoedas, moeda);
+        }
+
+        sc.close();
+    }
+}
