8bd480eef1ac98586dd8510bb244c5f10da5ce64
carolmedici <carolina.obstetriz@gmail.com>
2023-10-24 23:15:23 -0300

    Melhorando o código

--- curso_programacao/src/curso_programacao/ex60.java
+++ curso_programacao/src/curso_programacao/ex60.java
@@ -6,15 +6,28 @@ public class ex60 {
 
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
-
+		 	
+		 	System.out.println("Insira um número maior que 1: ");
 	        int N = sc.nextInt();
-
-	        for (int i = 1; i <= N; i++) {
-	            if (i % 2 == 0) {
-	                int square = i * i;
-	                System.out.println(i + "^2 = " + square);
-	            }
+	        
+	        while(N <= 1) {
+	        	System.out.println("Erro. Insira um número maior que 1: ");
+		        N = sc.nextInt();
 	        }
+	        
+	        System.out.println();
+	        
+	        
+		    System.out.println("O quadrado dos pares até " + N + " é:");
+		        
+		    for (int i = 1; i <= N; i++) {
+		       if (i % 2 == 0) {
+		          int square = i * i;
+		               
+		          System.out.println(i + "^2 = " + square);
+		        }
+		    }
+	        
 
 	        sc.close();
 	    }
