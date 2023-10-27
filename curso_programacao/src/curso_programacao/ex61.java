1d8b64330857e30d9fb1a8abd3c427e136b7139d
Carolina Médici <carolina.obstetriz@gmail.com>
2023-10-26 23:12:43 -0300

    Ajustes no exercício

--- curso_programacao/src/curso_programacao/ex61.java
+++ curso_programacao/src/curso_programacao/ex61.java
@@ -7,12 +7,14 @@ public class ex61 {
 	public static void main(String[] args) {
  
         Scanner sc = new Scanner(System.in);
+	System.out.println("Digite um número:");
          int N = sc.nextInt();
-        
+
+	System.out.println("Agora descubra a sequência lógica do exercício:");
         for (int i = 1; i <= N; i++) {
             int quadrado = i * i;
             int cubo = i * i * i;
-            
+           
             System.out.printf("%d %d %d%n", i, quadrado, cubo);
             System.out.printf("%d %d %d%n", i, quadrado + 1, cubo + 1);
         }
